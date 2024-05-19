0c0e3b403e547a2b0d1d4c94bbc9cbb29f3b92a6
markohautala <markohautala@live.se>
2024-05-19 17:36:59 +0200

    Update VSCode settings, Django database configuration, and CSRF_TRUSTED_ORIGINS in settings.py

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = ((0, "Draft"), (1, "Published")
+STATUS = (
+    (0, "Draft"), 
+    (1, "Published")
+)
 
 # Create your models here.
 class Post(models.Model):
--- codestar/settings.py
+++ codestar/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
-CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com"]
+CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com", "http://localhost:8000", "http://"]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
