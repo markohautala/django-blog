a82170e93aad20ec95b3829587f1e2959862bdab
markohautala <markohautala@live.se>
2024-05-26 12:06:41 +0200

    Add PostList view to display list of posts on the home page

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,5 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    model = Post
+    queryset = Post.objects.filter(status=1) # status=1 means that the post is published, status=0 means that the post is draft.
+    template_name = "post_list.html" # This is the template that will be used to render the view.
