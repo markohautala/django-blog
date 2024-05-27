38fd23374ef2e724da62cce026be69268f5244ec
markohautala <markohautala@live.se>
2024-05-27 21:32:48 +0200

    Fix issue: with summernote - debug is turned to true

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
@@ -46,14 +46,6 @@ INSTALLED_APPS = [
     'django_summernote',
     'blog',
 ]
-
-# Additional configuration if needed
-SUMMERNOTE_CONFIG = {
-    'iframe': True,  # Use iframe for the editor
-    'width': '100%',  # Editor width
-    'height': '480',  # Editor height
-    # Any additional Summernote settings you need
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -135,7 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
