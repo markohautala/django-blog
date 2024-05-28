4a69fbfb3ececcc093d1a4c6423f85a28d621cfb
markohautala <markohautala@live.se>
2024-05-28 20:49:09 +0200

    Add base.html and index.html and static files (css)

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,5 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1) # status=1 means that the post is published, status=0 means that the post is draft.
-    template_name = "post_list.html" # This is the template that will be used to render the view.
+    template_name = "blog/index.html"
+    paginate_by = 6
--- codestar/settings.py
+++ codestar/settings.py
@@ -20,6 +20,7 @@ CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com", "http://localhost:8000", "htt
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
